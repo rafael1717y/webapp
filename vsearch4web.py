29f61be10ade68777207b6b7b867ed783356272d
rafael1717y <rafael1717yy@gmail.com>
2022-03-27 18:39:17 -0300

    logs

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request, redirect
+from os import sep
+from flask import Flask, render_template, request, escape
 from mymodules.vsearch import search4letters
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> '302':  
-  return redirect('/entry')
+
+def log_request(req: 'flask_request', res: str) -> None:
+  with open('vsearch.log', 'a') as log:
+    #print(str(dir(req)), res, file=log)
+    print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 # --- /search4 ----
 # retorna o template e as variáveis obtidas no form.
+@app.route('/')
 @app.route('/search4', methods=['POST'])
 def do_search() -> str:
   phrase = request.form['phrase']
   letters = request.form['letters']
   title = 'Here your results:'
   results = str(search4letters(phrase, letters))
+  log_request(request, results)
   return render_template('results.html', 
                         the_phrase=phrase,
                         the_letters = letters,
@@ -31,6 +36,19 @@ def entry_page() -> 'html':
                          the_title="Welcome to search4letters on the web!")
 
 
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+  contents = []
+  with open('vsearch.log') as log:
+    for line in log:
+      contents.append([])
+      for item in line.split('|'):
+        contents[-1].append(escape(item))
+  titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+  return render_template('viewlog.html', the_title='View Log', the_row_titles=titles, the_data = contents, )
+    
 
 
-app.run(debug=True)
+
+if __name__ == '__main__':
+  app.run(debug=True)
