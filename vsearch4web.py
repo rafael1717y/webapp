b3e98985a50277ca50714408e0867780c0c14d83
rafael1717y <rafael1717yy@gmail.com>
2022-03-27 17:21:37 -0300

    templates and forms

---
+++ vsearch4web.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+from mymodules.vsearch import search4letters
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello() -> '302':  
+  return redirect('/entry')
+
+
+# --- /search4 ----
+# retorna o template e as variáveis obtidas no form.
+@app.route('/search4', methods=['POST'])
+def do_search() -> str:
+  phrase = request.form['phrase']
+  letters = request.form['letters']
+  title = 'Here your results:'
+  results = str(search4letters(phrase, letters))
+  return render_template('results.html', 
+                        the_phrase=phrase,
+                        the_letters = letters,
+                        the_title = title, 
+                        the_results = results,
+                         )
+
+
+@app.route('/entry')
+def entry_page() -> 'html':
+  return render_template('entry.html', 
+                         the_title="Welcome to search4letters on the web!")
+
+
+
+
+app.run(debug=True)
